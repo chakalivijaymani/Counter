2d22d2095ccee1bfdb8347d01fb9515ada32e3bc
chakalivijaymani <vijaymaniv10@gmail.com>
2024-06-26 01:05:20 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var result=0;
+var cnt=document.getElementById('counter');
+let rst =document.getElementById('reset');
+let incre = document.getElementById('increament');
+let decre= document.getElementById('decreament');
+
+rst.addEventListener('click', function(){
+    result=0;
+    cnt.textContent = result;
+    cnt.style.color='black';
+})
+incre.addEventListener('click', function(){
+    result+=1;
+    cnt.textContent = result;
+    cnt.style.color='green';
+
+});
+decre.addEventListener('click', function(){
+    result-=1;
+    cnt.textContent = result;
+    cnt.style.color='red';
+
+});
+
+
